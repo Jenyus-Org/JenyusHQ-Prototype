052bb7d8e1e5260989a9faa758e772dd7f13dbbd
Dan6erbond <moravrav@gmail.com>
2021-01-04 15:15:32 +0100

    Revert "Rename `theme.tsx` to `theme.ts`"
    This reverts commit 15152f6adbb118cb2ab496c926873885104be637.

--- client/src/theme.tsx
+++ client/src/theme.tsx
@@ -18,13 +18,13 @@ export const theme = createMuiTheme({
   overrides: {
     MuiAppBar: {
       root: {
-        boxShadow: "0 4px 10px 0 rgba(182, 170, 170, 0.3)",
+        boxShadow: "0 4px 10px 0 rgba(182, 170, 170, 0.15)",
       },
     },
     MuiDrawer: {
       paperAnchorDockedLeft: {
         borderRight: "none",
-        boxShadow: "0 4px 10px 0 rgba(182, 170, 170, 0.3)",
+        boxShadow: "0 4px 10px 0 rgba(182, 170, 170, 0.15)",
       },
     },
     MuiListItemIcon: {
@@ -46,7 +46,7 @@ export const theme = createMuiTheme({
     },
     MuiCard: {
       root: {
-        boxShadow: "0 5px 10px 2px rgba(182, 170, 170, 0.3)",
+        boxShadow: "0 5px 15px 5px rgba(182, 170, 170, 0.15)",
         borderRadius: "15px",
       },
     },
@@ -90,7 +90,7 @@ export const makeDarkTheme = (theme?: ThemeOptions) =>
       ...theme?.palette,
       divider: "#2e2e2e",
       background: {
-        ...purpleDarkTheme,
+        ...blueDarkTheme,
       },
       text: {
         primary: "#fff",
@@ -113,7 +113,7 @@ export const makeDarkTheme = (theme?: ThemeOptions) =>
           boxShadow: "0 4px 10px 0rgba(12, 12, 12, 0.8)",
         },
         colorDefault: {
-          backgroundColor: purpleDarkTheme.paper,
+          backgroundColor: blueDarkTheme.paper,
           color: "#eee",
         },
       },
